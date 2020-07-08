709758c2874c9dc6d6a4346f9ce9d00391145d47
haag <sim.paris@wanadoo.fr>
2020-07-08 08:28:51 +0200

    add parse log test

--- tests/test_log_reader.py
+++ tests/test_log_reader.py
@@ -1,5 +1,6 @@
 import unittest
 from src import log_reader
+from datetime import datetime
 
 class TestStringMethods(unittest.TestCase):
 
@@ -12,6 +13,49 @@ class TestStringMethods(unittest.TestCase):
 
 		section = log_reader.get_section('GET / HTTP/1.1')
 		self.assertEqual(section, '/')
+
+	def test_parse_log_line(self):
+		parsed_line = log_reader.parse_log_line(
+			'hostname zwkivs ojsbrx [08/Jul/2020:08:10:30 +0000] "GET http://my.site.com/home/ HTTP/1.1" 600 1375')
+		expected_parsed_line = {
+		'remote_host': 'hostname',
+		'user_identity': 'zwkivs',
+		'user_name': 'ojsbrx',
+		'datetime': datetime(2020, 7, 8, 8, 10, 30),
+		'request': 'GET http://my.site.com/home/ HTTP/1.1',
+		'status_code': 600,
+		'response_size': 1375,
+		'section': 'http://my.site.com/home'
+		}
+		self.assertDictEqual(parsed_line, expected_parsed_line)
+
+		parsed_line = log_reader.parse_log_line(
+			'hostname pttmsb scibil [08/Jul/2020:08:10:29 +0000] "HEAD / HTTP/1.1" 300 1002')
+		expected_parsed_line = {
+		'remote_host': 'hostname',
+		'user_identity': 'pttmsb',
+		'user_name': 'scibil',
+		'datetime': datetime(2020, 7, 8, 8, 10, 29),
+		'request': 'HEAD / HTTP/1.1',
+		'status_code': 300,
+		'response_size': 1002,
+		'section': '/'
+		}
+		self.assertDictEqual(parsed_line, expected_parsed_line)
+
+		parsed_line = log_reader.parse_log_line(
+			'hostname pttmsb scibil [08/Jul/2020:08:10:29 +0000] "HEAD / HTTP/1.1" 300 1002')
+		expected_parsed_line = {
+		'remote_host': 'hostname',
+		'user_identity': 'pttmsb',
+		'user_name': 'scibil',
+		'datetime': datetime(2020, 7, 8, 8, 10, 19),
+		'request': 'HEAD / HTTP/1.1',
+		'status_code': 300,
+		'response_size': 1002,
+		'section': 'http://my.site.com/home'
+		}
+		self.assertNotEqual(parsed_line, expected_parsed_line)
 
 
 if __name__ == '__main__':
